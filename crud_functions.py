5231329c735cf389cc2b69fa45dd63d795df0694
Dmitry <kerzhimanov@gmail.com>
2024-11-19 13:07:07 +0300

    final

--- crud_functions.py
+++ crud_functions.py
@@ -26,44 +26,10 @@ cursor.execute('CREATE INDEX IF NOT EXISTS idx_title ON Products (title)')
 cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
 
 
-# for i in range(1, 5):
-#     cursor.execute('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)',
-#                    (f'Продукт {i}', f'Описание {i}', i*100))
-
-# def add_users(username, email, age, balance):
-#     #is_included = cursor.execute('SELECT * FROM Users WHERE username = ?', (username, ))
-#     cursor.execute('''SELECT * FROM Users WHERE username = ?''', (str(username), ))
-#
-#     #if is_included.fetchone() is None:
-#     if cursor.fetchone() is None:
-#         #cursor.execute('''
-#          #INSERT INTO Users VALUES('{username}', '{email}', '{age}', '{balance}'''
-#          cursor.execute('''INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)''', (username, email, age, balance))
-# #     connection.commit()
-
-# def add_users(username, email, age, balance):
-#     is_included = cursor.execute('SELECT * FROM Users WHERE username = ?', (str(username),))
-#
-#     if is_included.fetchone() is None:
-#         cursor.execute(f'''
-#          INSERT INTO Users VALUES('{username}', '{email}', '{age}', '{balance}')
-#         ''')
-#     connection.commit()
-
-# def add_users(username, email, age, balance):
-#     is_included = cursor.execute('SELECT * FROM Users WHERE username = ?', (str(username),))
-#
-#     if is_included.fetchone() is None:
-#         cursor.execute(f'''
-#          INSERT INTO Users VALUES('{username}', '{email}', '{age}', '{balance}')
-#         ''')
-#     connection.commit()
-#     connection.close()
-
 
 def add_users(username, email, age, balance):
-    cursor.execute(f"INSERT INTO Users (username, email, age, balance ) VALUES(?, ?, ?, ?)",
-                   (f'{username}', f'{email}', age, balance))
+    cursor.execute('INSERT INTO Users (username, email, age, balance ) VALUES(?, ?, ?, ?)', (username, email, age, balance))
+    connection.commit()
 
 def is_included(username):
     check_user = cursor.execute('SELECT * FROM Users WHERE username=?', (username,))
@@ -84,13 +50,13 @@ def get_all_products():
     return all_products
 
 
-# def print_products():
+
 products = get_all_products()
 for product in products:
     # Выводим каждую строку в формате: id, title, description, price
     print(f'id: {product[0]}, title: {product[1]}, description: {product[2]}, price: {product[3]}')
 
-# print_products()
+
 
 connection.commit()
-#connection.close()
+
